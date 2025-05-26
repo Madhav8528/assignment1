f23c48077f9e2c47dc6dfee0b0c674008c4713d7
Madhav <madhavv8528@gmail.com>
2025-05-26 22:50:03 +0530

    updated db.js

--- config/db.js
+++ config/db.js
@@ -5,18 +5,34 @@ dotenv.config({
     path : "./.env"
 })
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 
-db.connect((err) => {
+connection.connect((err) => {
     if(err){
         throw err;
     }
     console.log("DB is connected successfully");
+
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS schools (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      address VARCHAR(255) NOT NULL,
+      latitude FLOAT NOT NULL,
+      longitude FLOAT NOT NULL
+    )
+  `;
+
+  connection.query(createTableQuery, (err) => {
+    if (err) throw err;
+    console.log("Schools table is ready");
+  });
+    
 })
 
-export default db;
+export default connection;
